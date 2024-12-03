6d2a707f8ce1533c0525e3875de4d7993129eef0
N1kZet <nkzt0709@gmail.com>
2024-12-03 15:44:06 +0500

    Update

--- Pr-12/json_test.py
+++ Pr-12/json_test.py
@@ -7,16 +7,16 @@ url = f"https://api.github.com/users/{username}"
 # делаем запрос и возвращаем json
 user_data1 = requests.get(url).json()
 
-pprint(user_data1)
+print(user_data1)
 print('-'*25)
 s1 = json.dumps(user_data1)
 user_data = json.loads(s1)
 info = {
         'company'   : (user_data["company"]   ),
-        'created_at': (user_data["created_at"]), 
-        'email'     : (user_data["email"]     ), 
-        'id'        : (user_data["id"]        ), 
-        'name'      : (user_data["name"]      ), 
+        'created_at': (user_data["created_at"]),
+        'email'     : (user_data["email"]     ),
+        'id'        : (user_data["id"]        ),
+        'name'      : (user_data["name"]      ),
         'url'       : (user_data["url"]       )
         }
 pprint(info)
--- Pr10/V1.py
+++ Pr10/V1.py
@@ -1,39 +1,22 @@
 #1
 n = 4
-arr = []
-#Заполняем массив элементами из файла data.txt
-file = open('data.txt')
-for i in file.readlines():
-    n1 = int(i)
-    arr.append(n1)
-arr = [arr]*n
-for i in arr:
+a = []
+with open("/home/n1kzet/PL-Krylov/Pr10/data.txt") as file:
+    for line in file:
+        arr = [int(x.strip()) for x in line.split()]
+        a.append(arr)
+
+print('Исходный массив:')
+for i in a:
     print(*i)
-max_e = -float('inf')
-min_e = +float('inf')
-max_index = [0,0]
-min_index = [0,0]
-#Нахождение max и min числа и индекса
-for i in range(n):
+
+for i in range(n-1):
     for j in range(n):
-        if arr[i][j]>max_e:
-            max_e=arr[i][j]
-            max_index = ([i,j])
-        if arr[i][j]<min_e:
-            min_e = arr[i][j]
-            min_index = ([i,j])
-#Замена max и min индекса
-row1,row2 = max_index[0],max_index[1]
-row3,row4 = min_index[0],min_index[1]
-arr[row1][row2],arr[row3][row4]=arr[row3][row4],arr[row1][row2]
-#print(max_e,max_index)
-#print(min_e,min_index)
+        a[i][j] -=a[n-1][j]
+
 print('-'*8)
-#Добавляем результат в файл file_vivod.txt
-file_vivod = arr
-#print(file_vivod)
 file_v = open('file_vivod.txt','w')
-file_v.write(str(file_vivod))
+file_v.write(str(a))
 file_v.close()
-for i in arr:
+for i in a:
     print(*i)
